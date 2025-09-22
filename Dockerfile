b859df998e82c06deda274cfdb35a8709b87d248
roman <roman121998yarkov@gmail.com>
2025-09-22 17:02:38 +0400

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
-FROM mcr.microsoft.com/dotnet/sdk:8.0 as build
-WORKDIR /app
-COPY . .
-WORKDIR /app
-RUN dotnet publish -c Release -o /app/publish
+#FROM mcr.microsoft.com/dotnet/sdk:8.0 as build
+#WORKDIR /app
+#COPY . .
+#WORKDIR /app
+#RUN dotnet publish -c Release -o /app/publish
+#
+#FROM mcr.microsoft.com/dotnet/aspnet:8.0 as final
+#WORKDIR /app
+#COPY --from=build /app/publish .
+#ENTRYPOINT ["dotnet", "Project 3.0.dll"]
+FROM mysql:8.0
 
-FROM mcr.microsoft.com/dotnet/aspnet:8.0 as final
-WORKDIR /app
-COPY --from=build /app/publish .
-ENTRYPOINT ["dotnet", "Project 3.0.dll"]
+# Устанавливаем переменные окружения для MySQL
+ENV MYSQL_ROOT_PASSWORD=
+ENV MYSQL_DATABASE=ComopanyProject
+
+# Копируем дамп базы данных в специальную директорию MySQL
+COPY damp_db.sql /docker-entrypoint-initdb.d/
--- Project 3.0/Program.cs
+++ Project 3.0/Program.cs
@@ -10,7 +10,7 @@ namespace Project_3._0
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews();            
             builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
             builder.Host.ConfigureContainer<ContainerBuilder>(cont =>
             {
@@ -37,13 +37,13 @@ namespace Project_3._0
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
+            builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
-        
             var app = builder.Build();
             // app.MapGet("/", () => "Hello World!");
             // app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             app.UseStaticFiles();
-         
+    
             app.MapControllers();
             app.UseSession();
 
